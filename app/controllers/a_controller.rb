7f2fed6bf1ee786775ed87be3c4d4ab8a9a81432
Sumeet Jain <sumeet@sumeetjain.com>
2016-02-09 11:53:56 -0600

    Some new controller actions and operations

--- app/controllers/a_controller.rb
+++ app/controllers/a_controller.rb
@@ -1,3 +1,13 @@
+# Processes the movie form.
+MyApp.get "/add_movie" do
+  x = Movie.new
+  x.title = params[:movie_title]
+  x.director_id = params[:director]
+  x.save
+
+  erb :"a/success_movie"
+end
+
 # Processes the form to add a director.
 MyApp.get "/add_director" do  
   x = Director.new
--- app/controllers/b_controller.rb
+++ app/controllers/b_controller.rb
@@ -6,3 +6,15 @@ end
 MyApp.get "/directors/creating" do
   erb :"b/new_director"
 end
+
+MyApp.get "/directors" do
+  @directors = Director.all
+
+  erb :"b/all_directors"
+end
+
+# Shows the form for adding a director.
+MyApp.get "/movies/creating" do
+  @directors = Director.all
+  erb :"b/new_movie"
+end
