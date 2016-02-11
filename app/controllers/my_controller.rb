cab866d01d7adcb34ef420cf4a55e1f3ed365775
Sumeet Jain <sumeet@sumeetjain.com>
2016-02-11 11:19:57 -0600

    Updating records. Also table-relationship methods.

--- app/controllers/my_controller.rb
+++ app/controllers/my_controller.rb
@@ -1,3 +1,20 @@
+MyApp.get "/process_edit_movie_form/:potato" do
+  @movie = Movie.find_by_id(params[:potato])
+
+  # Alternative
+  # @movie.update_attributes(...)
+
+  @movie.title = params["banana_title"]
+  @movie.director_id = params["potato_number"]
+  @movie.save
+  erb :"updated"
+end
+
+MyApp.get "/form_for_editing_movie/:banana" do
+  @movie = Movie.find_by_id(params[:banana])
+  erb :"edit_movie_form"
+end
+
 MyApp.get "/all_movies" do
   @movies = Movie.all
   erb :"movies_list"
